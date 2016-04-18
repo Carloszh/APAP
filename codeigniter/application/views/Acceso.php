10231ed51e2e9938350d94b000dbf2e59b892f04
DavidArcos <kruzpel6@gmail.com>
2016-04-17 20:11:25 -0500

    Revert "Revert "Modulo Login""
    This reverts commit ff3a3c2a0b79cf434d04ea42140b98011b9c1717.

---
+++ codeigniter/application/views/Acceso.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>APAP</title>
+        <link href="<?php echo base_url();?>css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo base_url();?>css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo base_url();?>css/bootstrap-theme.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo base_url();?>css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo base_url();?>css/estilos.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+
+<div id="container" align="center">
+    <div class="bienvenido">
+        <h1>Bienvenido</h1>
+    </div>
+    <div class="btn-group type-access">
+        <button type="button" class="btn btn-primary"><a href="<?php echo base_url();?>Login">Profesor</a></button>
+      <button type="button" class="btn btn-primary"><a href="<?php echo base_url();?>LoginAlumno">Alumno</a></button>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ codeigniter/application/views/Login.php
@@ -0,0 +1,41 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>APAP</title>
+        <link href="<?php echo base_url();?>css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo base_url();?>css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo base_url();?>css/bootstrap-theme.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo base_url();?>css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo base_url();?>css/estilos.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+
+<div id="container" align="center">
+    <div class="bienvenido">
+        <h1>Bienvenido</h1>
+        <h3>Acceso a Profesores</h3>
+    </div>
+    <div id="login-ptc">
+        
+        <?php echo validation_errors();?>
+        <form class="form-inline" role="form" method="POST" action="<?php echo base_url();?>Login/loginPtc">
+             <div class="form-group">
+               <label class="sr-only" >Clave</label>
+               <input type="text" class="form-control" name="clave"
+                      placeholder="Introduce Clave">
+             </div>
+             <div class="form-group">
+               <label class="sr-only" >Contraseña</label>
+               <input type="password" class="form-control" name="password"
+                      placeholder="Contraseña">
+             </div>
+             <button type="submit" class="btn btn-primary">Entrar</button>
+         </form>
+    </div>
+</div>
+
+</body>
+</html>
