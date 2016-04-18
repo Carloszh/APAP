9d2790ec69c4a70ea0ccb5191710708c65b3ab48
xSata <satanael.franco@gmail.com>
2016-04-18 18:56:50 -0500

    Modules Funciones DC

---
+++ funcionesDC/application/views/consultarInfoAlumno.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Consultar Datos Alumno</title>
+</head>
+<body>
+	<div align="center">
+		<form class="form-inline" role="form" method="POST" action="<?php echo base_url();?>index.php/consultarInfoAlumno">
+      <div class="form-group">
+        <label class="sr-only" >Matricula: </label>
+        <input type="text" class="form-control" name="matricula"
+          placeholder="Ingrese Matricula de Usuario">
+        <button type="submit" name="submit" class="btn btn-primary">Buscar</button>       
+      </div>     
+    </form>
+	</div>
+
+</body>
+</html>
